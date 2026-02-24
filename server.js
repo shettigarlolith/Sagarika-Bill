bdc70b2ddd36d7b138b09b93dda5524cc71c964f
NEW_GITHUB_USERNAME <NEW_EMAIL@example.com>
2026-02-24 18:25:30 +0530

    Enable CORS for cross-origin frontend API calls

--- server.js
+++ server.js
@@ -49,6 +49,22 @@ if (!fs.existsSync(dataDir)) {
 }
 
 app.use(express.json());
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  return next();
+});
 app.use(
   express.static(path.join(__dirname, 'public'), {
     etag: false,
